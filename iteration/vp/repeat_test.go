a5dd5334fe9c6923a24b1c7fecb21f026650dc20
mihozaki <mihoubiadam7@gmail.com>
2024-02-11 20:38:18 +0100

    Updated test to match repeat fun new repetition argument

--- iteration/vp/repeat_test.go
+++ iteration/vp/repeat_test.go
@@ -1,17 +1,20 @@
 package iteration
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 6)
+	expected := strings.Repeat("a", 6)
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 6)
 	}
 
 }
