f127f6155fc9b6bf7bb3db168e9917fcb263ff46
mihozaki <mihoubiadam7@gmail.com>
2024-02-11 11:27:29 +0100

    idk

--- integers/add.go
+++ integers/add.go
@@ -2,8 +2,12 @@ package integers
 
 func Add(x, y int) int {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	return x + y
 =======
 	return 4
 >>>>>>> c224416 (Added the Add function and its test)
+=======
+	return x + y
+>>>>>>> dc4717f (Refactored the add function to use a proper return expression)
 }
