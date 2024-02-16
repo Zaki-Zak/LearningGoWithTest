42320d95b55ad1db4cc8342ace90bb5c58931a02
mihozaki <mihoubiadam7@gmail.com>
2024-02-16 19:53:23 +0100

    Update Add to handle existing words addition

---
+++ maps/v5/dictionary_test.go
@@ -0,0 +1,56 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+	t.Run("uknown word", func(t *testing.T) {
+		_, got := dictionary.Search("uknown")
+		assertErrors(t, got, ErrNotFound)
+	})
+}
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+
+		dictionary := Dictionary{}
+		definition := "this is just a test"
+		want := "test"
+		err := dictionary.Add(want, definition)
+		assertErrors(t, err, nil)
+		assertDefinition(t, dictionary, want, definition)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+		assertErrors(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+func assertErrors(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
+}
