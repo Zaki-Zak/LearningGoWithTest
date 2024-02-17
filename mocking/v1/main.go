9c536cef0a5973436f6f551e17a534e06098eb07
mihozaki <mihoubiadam7@gmail.com>
2024-02-17 09:44:33 +0100

    Add function call in main with stdout

--- mocking/v1/main.go
+++ mocking/v1/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Countdown(out *bytes.Buffer) {
+func Countdown(out io.Writer) {
 	fmt.Fprint(out, "3")
 }
 func main() {
+	Countdown(os.Stdout)
 }
