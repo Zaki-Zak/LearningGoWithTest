bbb8879814e927598ad258b799bc3633824aea10
mihozaki <mihoubiadam7@gmail.com>
2024-02-19 18:50:10 +0100

    Added concurrency with goroutines and channels

--- concurrency/v2/check_website.go
+++ concurrency/v2/check_website.go
@@ -1,17 +1,23 @@
 package concurrency
 
-import "time"
-
 type WebsiteCheck func(string) bool
+type result struct {
+	index    string
+	validity bool
+}
 
 func CheckWebsites(wc WebsiteCheck, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultchannel := make(chan result)
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			resultchannel <- result{u, wc(u)}
 		}(url)
 
 	}
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultchannel
+		results[r.index] = r.validity
+	}
 	return results
 }
