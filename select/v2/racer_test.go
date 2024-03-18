9dee95f9e82526f15bda01bac43d898646f6b8fa
MihoZaki <mihoubiadam7@gmail.com>
2024-03-18 18:21:03 +0100

    Added a concurrent test using select

---
+++ select/v2/racer_test.go
@@ -0,0 +1,32 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayServer(20 * time.Millisecond)
+	fastServer := makeDelayServer(0 * time.Millisecond)
+	defer fastServer.Close()
+	defer slowServer.Close()
+
+	slowURL := slowServer.URL
+
+	fastURL := fastServer.URL
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func makeDelayServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
